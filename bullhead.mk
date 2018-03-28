0891b551ff8dc361490b981292b65b8cc12f838c
argraur <argraur@gmail.com>
2018-03-28 18:26:52 +0000

    bullhead: Nuke AOSP Launcher and WallpaperPicker
    We have replacements for them so nuke them
    Signed-off-by: argraur <argraur@gmail.com>

--- bullhead.mk
+++ bullhead.mk
@@ -37,8 +37,3 @@ $(call inherit-product, device/lge/bullhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/bullhead/bullhead-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixel2rom/config/common_full_phone.mk)
 $(call inherit-product-if-exists, vendor/gapps/gapps.mk)
-
-PRODUCT_PACKAGES += \
-    Launcher3 \
-    WallpaperPicker
-
